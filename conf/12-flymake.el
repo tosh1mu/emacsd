ad3f12b12e6b38ada0b3740d51c06b8c96dc43a8
tosh914 <pandora87@tauhat.com>
2012-12-11 04:51:38 +0900

    enabled pop-up message for flymake

--- conf/12-flymake.el
+++ conf/12-flymake.el
@@ -2,6 +2,27 @@
 ; flymake settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when (require 'flymake nil t)
+  (when (require 'popup nil t)
+	(defun flymake-display-err-menu-for-current-line ()
+	  (interactive)
+	  (let* ((line-no             (flymake-current-line-no))
+			 (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no))))
+		(when line-err-info-list
+		  (let* ((count           (length line-err-info-list))
+				 (menu-item-text  nil))
+			(while (> count 0)
+			  (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
+			  (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+					 (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+				(if file
+					(setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
+			  (setq count (1- count))
+			  (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
+			  )
+			(popup-tip menu-item-text)))))
+	(global-set-key (kbd "C-c p") 'flymake-display-err-menu-for-current-line))
+
+
   (defun flymake-cc-init ()
 	(let* ((temp-file   (flymake-init-create-temp-buffer-copy
 						 'flymake-create-temp-inplace))
@@ -16,4 +37,5 @@
   
   (add-hook 'c++-mode-hook
 			'(lambda ()
-			   (flymake-mode t))))
+			   (flymake-mode t)))
+)

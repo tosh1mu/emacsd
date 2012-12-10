187bb3b8d3d394f9eade5d1ee3b15a9bdd3b6a84
tosh914 <pandora87@tauhat.com>
2012-12-11 01:45:06 +0900

    added settings for STRATOS.local

--- conf/99-machine.el
+++ conf/99-machine.el
@@ -1,16 +1,24 @@
-(when window-system (cond(
+(when window-system
+  (cond
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; TAKA ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (equal system-name "TAKA")
-  (progn
-	(if (>= (x-display-pixel-width) 2000)
-		(progn (setq width-gain 0.3) (setq lpos 2000))
-	  (progn (setq width-gain 0.5) (setq lpos 50)))
-	(setq height-gain 0.8)
-	(setq fwidth (floor (/ (* (x-display-pixel-width) width-gain) (frame-char-width))))
-	(setq fheight (floor (/ (* (x-display-pixel-height) height-gain) (frame-char-height))))
-	(setq tpos 50)
-	(set-frame-size (selected-frame) fwidth fheight)
-	(set-frame-position (selected-frame) lpos tpos))
-  )))
+   ((equal system-name "TAKA")
+	(progn
+	  (if (>= (x-display-pixel-width) 2000)
+		  (progn (setq width-gain 0.3) (setq lpos 2000))
+		(progn (setq width-gain 0.5) (setq lpos 50)))
+	  (setq height-gain 0.8)
+	  (setq fwidth (floor (/ (* (x-display-pixel-width) width-gain) (frame-char-width))))
+	  (setq fheight (floor (/ (* (x-display-pixel-height) height-gain) (frame-char-height))))
+	  (setq tpos 50)
+	  (set-frame-size (selected-frame) fwidth fheight)
+	  (set-frame-position (selected-frame) lpos tpos)))	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; STORATOS.local ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   ((equal system-name "STRATOS.local")
+	(progn
+	  (set-frame-size (selected-frame) 100 50)
+	  (set-frame-position (selected-frame) 20 40)))
+))

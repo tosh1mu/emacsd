f9f6a8174da04978dd004089a5d7d73d22553c9a
tosh914 <pandora87@tauhat.com>
2012-12-11 05:00:43 +0900

    fixed c-mode setting

--- conf/13-c-mode.el
+++ conf/13-c-mode.el
@@ -2,8 +2,11 @@
 ; C-mode setting ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'c-mode-common-hook
+		  (lambda ()
+			))
+(add-hook 'c-mode-common-hook
           '(lambda ()
-			 (c-set-style "java")
+			 ;; (c-set-style "java")
 										; センテンスの終了である ';' を入力したら、自動改行+インデント
 			 ;; (c-toggle-auto-hungry-state 1)
 										; RET キーで自動改行+インデント
@@ -14,9 +17,13 @@
 (add-hook 'c++-mode-hook
           '(lambda()
              (c-set-style "cc-mode")
-			 (setq c-auto-newline 0)
+			 (setq c-auto-newline nil)
 			 (setq c-tab-always-indent t)
 			 (setq indent-tabs-mode t)     ; インデントは空白文字で行う（TABコードを空白に変換）
              (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
              (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
 			 ))
+
+
+
+

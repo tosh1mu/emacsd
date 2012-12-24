fc55816fcd01aca5f121399ce569a463801c3744
tosh@TAKA <pandora87@tauhat.com>
2012-12-24 22:20:02 +0900

    tentative

--- conf/13-c-mode.el
+++ conf/13-c-mode.el
@@ -2,12 +2,16 @@
 ; C-mode setting ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'c-mode-common-hook
-		  (lambda ()
-			))
-(add-hook 'c-mode-common-hook
           '(lambda ()
+			 (c-set-style "cc-mode")
 			 ;; (c-set-style "java")
 										; センテンスの終了である ';' を入力したら、自動改行+インデント
+			 (setq c-auto-newline nil)
+			 (setq c-tab-always-indent t)
+			 (setq indent-tabs-mode t)
+										; インデントは空白文字で行う（TABコードを空白に変換）
+             (c-set-offset 'arglist-close 0)
+										; 関数の引数リストの閉じ括弧はインデントしない
 			 ;; (c-toggle-auto-hungry-state 1)
 										; RET キーで自動改行+インデント
              (define-key c-mode-base-map "\C-m" 'newline-and-indent)
@@ -19,9 +23,13 @@
              (c-set-style "cc-mode")
 			 (setq c-auto-newline nil)
 			 (setq c-tab-always-indent t)
-			 (setq indent-tabs-mode t)     ; インデントは空白文字で行う（TABコードを空白に変換）
-             (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
-             (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
+			 (setq indent-tabs-mode t)
+										; インデントは空白文字で行う（TABコードを空白に変換）
+             (c-set-offset 'innamespace 0)
+										; namespace {}の中はインデントしない
+             (c-set-offset 'arglist-close 0)
+										; 関数の引数リストの閉じ括弧はインデントしない
+			 (define-key c-mode-base-map "\C-m" 'newline-and-indent)
 			 ))
 
 

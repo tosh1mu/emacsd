4654a9deacd1749eb36d9f42cc5f8846a17a2668
tosh@TASP <pandora87@tauhat.com>
2013-01-04 20:40:52 +0900

    changed setting for TASP

--- conf/99-machine.el
+++ conf/99-machine.el
@@ -26,8 +26,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
    ((equal system-name "TASP")
 	(progn
-	  (setq width-gain 0.75)
-	  (setq height-gain 0.75)
+	  (if (>= (x-display-pixel-width) 2000)
+		  (progn (setq lpos 100) (setq width-gain 0.3) (setq height-gain 0.6))
+		(progn (setq lpos 50) (setq width-gain 0.5) (setq height-gain 0.8)))
 	  (setq fwidth (floor (/ (* (x-display-pixel-width) width-gain) (frame-char-width))))
 	  (setq fheight (floor (/ (* (x-display-pixel-height) height-gain) (frame-char-height))))
 	  (setq lpos 50)
